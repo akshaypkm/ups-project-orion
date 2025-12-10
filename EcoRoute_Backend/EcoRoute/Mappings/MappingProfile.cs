08fc8b54e63dc25c6643690bf8a2be8aa6ec5b4a
akshaypkm <akshay.g.2022.cse@ritchennai.edu.in>
2025-12-10 16:42:52 +0530

    DTOS migrated to new folder

---
+++ EcoRoute_Backend/EcoRoute/Mappings/MappingProfile.cs
@@ -0,0 +1,12 @@
+using AutoMapper;
+
+namespace EcoRoute.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // CreateMap<
+        }
+    }
+}
