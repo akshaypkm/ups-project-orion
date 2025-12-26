0f80358a16554f4683f0d430c19db06e3bbbf326
akshaypkm <akshay.g.2022.cse@ritchennai.edu.in>
2025-12-26 15:37:07 +0530

    plenty of changes in tables

---
+++ EcoRoute_Backend/EcoRoute/Migrations/20251226061052_RemovedMontlyCreditsListCreatedRemainingCredits.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcoRoute.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedMontlyCreditsListCreatedRemainingCredits : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MonthlyEmissionsCap",
+                table: "Companies",
+                newName: "RemainingCredits");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RemainingCredits",
+                table: "Companies",
+                newName: "MonthlyEmissionsCap");
+        }
+    }
+}
