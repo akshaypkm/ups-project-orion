d7cb30d5e8ab4e73efaed476c9a842d9a00e38ae
akshaypkm <akshay.g.2022.cse@ritchennai.edu.in>
2026-01-02 08:16:34 +0530

    changes in order table

---
+++ EcoRoute_Backend/EcoRoute/Migrations/20251227181105_IsAutoApprovedPropAdded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcoRoute.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsAutoApprovedPropAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAutoApproved",
+                table: "Orders",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAutoApproved",
+                table: "Orders");
+        }
+    }
+}
